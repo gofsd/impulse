a44b2e1215007ee8176fb35536b5335ec957ef56
Dima Pohyba <madi.nickname@gmail.com>
2020-06-27 17:18:46 +0300

    add crud decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { Crud, CrudController } from '@nestjsx/crud';
+import Users from './user.entity'
+import { UsersService } from './users.service'
 
+@Crud({
+    model: {
+      type: Users,
+    }
+  })
 @Controller('users')
-export class UsersController {}
+export class UsersController  implements CrudController<Users>{
+    constructor(public service: UsersService){}
+}
